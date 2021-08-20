e1efa67500baa5d1795d50839c02afc6aafa6d30
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-08-20 13:58:35 -0400

    trying to fix error in accessing static directory

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require('path')
+// const path = require('path')
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname, + '/public'));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
